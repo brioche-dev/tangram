f4e538ab317df53507710c5a33ccd122b9f9304c
Isabella Tromba <isabella.tromba@gmail.com>
2022-09-09 20:44:13 -0400

    tg run for registry dependency

--- src/cli/commands/run.rs
+++ src/cli/commands/run.rs
@@ -1,5 +1,5 @@
 use crate::config::Config;
-use anyhow::{bail, Context, Result};
+use anyhow::{anyhow, bail, Context, Result};
 use clap::Parser;
 use tangram::{
 	client::Client,
@@ -7,6 +7,7 @@ use tangram::{
 };
 
 #[derive(Parser, Debug)]
+#[clap(trailing_var_arg = true)]
 pub struct Args {
 	#[clap(long)]
 	executable_path: Option<String>,
@@ -16,6 +17,7 @@ pub struct Args {
 	name: String,
 	#[clap(default_value = ".")]
 	package: String,
+	trailing_args: Vec<String>,
 }
 
 pub async fn run(args: Args) -> Result<()> {
@@ -34,36 +36,54 @@ pub async fn run(args: Args) -> Result<()> {
 		.context("Failed to parse the package specifier.")?;
 
 	// Evaluate and checkout the resulting artifact.
-	let artifact = match package_specifier {
+	let package = match package_specifier {
 		Specifier::Path(PathSpecifier { path }) => {
 			// Checkin the package.
-			let package = client
+			client
 				.checkin_package(&path, args.locked)
 				.await
-				.context("Failed to check in the package.")?;
-
-			// Create the expression.
-			let expression = tangram::expression::Expression::Target(tangram::expression::Target {
-				lockfile: None,
-				package,
-				name: args.name.clone(),
-				args: vec![],
-			});
-
-			// Evaluate the expression.
-			let value = client
-				.evaluate(expression)
+				.context("Failed to check in the package.")?
+		},
+		Specifier::Registry(RegistrySpecifier {
+			package_name,
+			version,
+		}) => {
+			// TODO get rid of this requirement once we figure out the kv store.
+			let version = version.ok_or_else(|| anyhow!("For now you must pass a version."))?;
+			// Get the package from the registry.
+			client
+				.get_package(&package_name, &version)
 				.await
-				.context("Failed to evaluate the target expression.")?;
+				.with_context(|| {
+					format!("Failed to get the package with name {package_name} from the registry.")
+				})?
+				.ok_or_else(|| {
+					anyhow!(format!(
+						"Failed to get the package with name {package_name}."
+					))
+				})?
+		},
+	};
 
-			match value {
-				tangram::value::Value::Artifact(artifact) => artifact,
-				_ => bail!("Failed to run. The provided target must evaluate to an artifact in order to be run."),
-			}
-		},
-		Specifier::Registry(RegistrySpecifier { .. }) => {
-			todo!()
-		},
+	// Create the expression.
+	let expression = tangram::expression::Expression::Target(tangram::expression::Target {
+		lockfile: None,
+		package,
+		name: args.name.clone(),
+		args: vec![],
+	});
+
+	// Evaluate the expression.
+	let value = client
+		.evaluate(expression)
+		.await
+		.context("Failed to evaluate the target expression.")?;
+
+	let artifact = match value {
+		tangram::value::Value::Artifact(artifact) => artifact,
+		_ => bail!(
+			"Failed to run. The provided target must evaluate to an artifact in order to be run."
+		),
 	};
 
 	// Check that the client is connected to an in-process server.
@@ -88,7 +108,9 @@ pub async fn run(args: Args) -> Result<()> {
 	};
 
 	// Run the process!
-	let mut child = tokio::process::Command::new(&executable_path).spawn()?;
+	let mut child = tokio::process::Command::new(&executable_path)
+		.args(args.trailing_args)
+		.spawn()?;
 	child.wait().await?;
 
 	Ok(())
