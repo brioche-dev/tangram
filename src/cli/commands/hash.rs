618c1b9d4a92bb5e6e248cde6294a92258e1aa46
David Yamnitsky <david@yamnitsky.com>
2022-10-04 09:47:49 -0400

    use reqwest instead of hyper in hash

--- src/cli/commands/hash.rs
+++ src/cli/commands/hash.rs
@@ -1,4 +1,4 @@
-use anyhow::{bail, Context, Result};
+use anyhow::{Context, Result};
 use clap::Parser;
 use futures::TryStreamExt;
 use tokio::io::AsyncRead;
@@ -13,39 +13,16 @@ pub async fn run(args: Args) -> Result<()> {
 	// Create the hasher.
 	let mut hasher = tangram::hash::Hasher::new();
 
-	// Create the HTTP client.
-	let client: hyper::Client<_, hyper::Body> = hyper::Client::builder().build(
-		hyper_rustls::HttpsConnectorBuilder::new()
-			.with_native_roots()
-			.https_or_http()
-			.enable_http1()
-			.build(),
-	);
-
 	// Get the input.
 	let mut input: Box<dyn AsyncRead + Unpin + Send + Sync> = if let Some(input) = args.input {
 		if let Ok(input) = Url::parse(&input) {
 			// Perform the request.
-			let request = http::Request::builder()
-				.uri(input.to_string())
-				.body(hyper::Body::empty())
-				.unwrap();
-			let response = client.request(request).await?;
-
-			// Handle a non-success status.
-			if !response.status().is_success() {
-				let status = response.status();
-				let body = hyper::body::to_bytes(response.into_body())
-					.await
-					.context("Failed to read the response body.")?;
-				let body = String::from_utf8(body.to_vec())
-					.context("Failed to read the response body as a string.")?;
-				bail!("{status}\n{body}");
-			}
+			let response = reqwest::get(input).await?;
+			let response = response.error_for_status()?;
 
 			// Create a reader from the stream.
 			let response = response
-				.into_body()
+				.bytes_stream()
 				.map_err(|error| std::io::Error::new(std::io::ErrorKind::Other, error));
 			let response = tokio_util::io::StreamReader::new(response);
 			Box::new(response)
