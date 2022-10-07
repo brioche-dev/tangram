43ea359d8032baf564c312107ebfae69b80266bd
William Goodall <wgoodall01@gmail.com>
2022-10-06 23:30:42 -0400

    alias.bash: update for new tangram_cli package name

--- scripts/alias.bash
+++ scripts/alias.bash
@@ -5,7 +5,7 @@ __tg_alias_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && p
 # Run function in subshell
 __tg_alias_run() (
 	set -e
-	(cd "$__tg_alias_dir"; cargo build --package tangram --bin tg --quiet)
+	(cd "$__tg_alias_dir"; cargo build --package tangram_cli --bin tg --quiet)
 	"$__tg_alias_dir/target/debug/tg" "$@"
 )
 
