56c1162b3db8c2da3b391a536f53de71cb6a5aee
Kyle Lacy <kylelacy@kyle.space>
2022-10-07 18:16:28 -0700

    update Linux sandbox to run even if setting up container fails

--- packages/core/evaluators/process/linux.rs
+++ packages/core/evaluators/process/linux.rs
@@ -5,7 +5,7 @@ use crate::{
 };
 
 use super::{SandboxPathMode, SandboxedCommand};
-use anyhow::{anyhow, bail, Context, Result};
+use anyhow::{bail, Context, Result};
 use bstr::ByteSlice;
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use libc::*;
@@ -178,39 +178,61 @@ fn pre_exec(
 	child_socket: &mut std::os::unix::net::UnixStream,
 	parent_child_root_path: &Path,
 	command: &SandboxedCommand,
-) -> Result<()> {
+) -> std::io::Result<()> {
+	let result = set_up_sandbox(child_socket, parent_child_root_path, command);
+	match result {
+		Ok(()) => Ok(()),
+		Err(SandboxError::Incomplete(error)) => {
+			// Print a warning if the sandbox setup did not finish completely.
+			eprintln!("Warning: Sandbox setup failed. {error:#}");
+			Ok(())
+		},
+		Err(error) => {
+			// Print the error and bail if sandbox setup returned a fatal error. We print it here because the failures in `pre_exec` always shows up as `EINVAL` to the caller if there's no OS error number associated with it.
+			eprintln!("An error occurred while trying to setup the sandbox. {error:#}");
+			Err(std::io::Error::new(std::io::ErrorKind::Other, error))
+		},
+	}
+}
+
+fn set_up_sandbox(
+	child_socket: &mut std::os::unix::net::UnixStream,
+	parent_child_root_path: &Path,
+	command: &SandboxedCommand,
+) -> Result<(), SandboxError> {
 	// Unshare the user namespace.
 	let ret = unsafe { unshare(CLONE_NEWUSER) };
 	if ret != 0 {
-		bail!(anyhow!(std::io::Error::last_os_error())
-			.context("Failed to unshare the user namepsace."));
+		return Err(SandboxIncomplete::UnshareUser(std::io::Error::last_os_error()).into());
 	}
 
 	// Send the message to the parent process that the UID and GID maps are ready to be set.
 	let pid = unsafe { getpid() };
-	child_socket.write_i32::<BigEndian>(pid).context("Failed to send the message to the parent process that the UID and GID maps are ready to be set.")?;
+	child_socket
+		.write_i32::<BigEndian>(pid)
+		.map_err(SandboxError::FailedToSendUidMapMessage)?;
 
 	// Wait for the message from the parent process that the UID and GID maps have been set.
-	child_socket.read_u8().context("Failed to receive the message from the parent process that the UID and GID maps have been set.")?;
+	child_socket
+		.read_u8()
+		.map_err(SandboxError::FailedToReceiveUidMapMessage)?;
 
 	// Set the UID and GID.
 	let uid = 0;
 	let ret = unsafe { setresuid(uid, uid, uid) };
 	if ret != 0 {
-		bail!(anyhow!(std::io::Error::last_os_error()).context("Failed to set the uid."));
+		return Err(SandboxIncomplete::SetUidFailed(std::io::Error::last_os_error()).into());
 	}
 	let gid = 0;
 	let ret = unsafe { setresgid(gid, gid, gid) };
 	if ret != 0 {
-		bail!(anyhow!(std::io::Error::last_os_error()).context("Failed to set the gid."));
+		return Err(SandboxIncomplete::SetGidFailed(std::io::Error::last_os_error()).into());
 	}
 
 	// Unshare the mount namespace.
 	let ret = unsafe { unshare(CLONE_NEWNS) };
 	if ret != 0 {
-		bail!(
-			anyhow!(std::io::Error::last_os_error()).context("Failed to unshare mount namespace.")
-		);
+		return Err(SandboxIncomplete::UnshareMount(std::io::Error::last_os_error()).into());
 	}
 
 	// Ensure the parent child root path does not have shared propagation.
@@ -226,8 +248,7 @@ fn pre_exec(
 		)
 	};
 	if ret != 0 {
-		bail!(anyhow!(std::io::Error::last_os_error())
-			.context("Failed to ensure the new root does not have shared propagation."));
+		return Err(SandboxIncomplete::MountPrivateRoot(std::io::Error::last_os_error()).into());
 	}
 
 	// Ensure the parent child root path is a mount point.
@@ -243,8 +264,7 @@ fn pre_exec(
 		)
 	};
 	if ret != 0 {
-		bail!(anyhow!(std::io::Error::last_os_error())
-			.context("Failed to ensure the new root is a mount point."));
+		return Err(SandboxIncomplete::MountBindRoot(std::io::Error::last_os_error()).into());
 	}
 
 	// Create the parent mount path.
@@ -269,7 +289,9 @@ fn pre_exec(
 		)
 	};
 	if ret != 0 {
-		bail!(anyhow!(std::io::Error::last_os_error()).context("Failed to mount /proc."));
+		return Err(
+			SandboxIncomplete::MountFailed(std::io::Error::last_os_error(), parent_proc).into(),
+		);
 	}
 
 	// Mount /dev.
@@ -288,12 +310,14 @@ fn pre_exec(
 		)
 	};
 	if ret != 0 {
-		bail!(anyhow!(std::io::Error::last_os_error()).context("Failed to mount /dev."));
+		return Err(
+			SandboxIncomplete::MountFailed(std::io::Error::last_os_error(), parent_dev).into(),
+		);
 	}
 
 	// Create /tmp.
 	let child_tmp_path = parent_child_root_path.join("tmp");
-	std::fs::create_dir_all(&child_tmp_path)?;
+	std::fs::create_dir_all(&child_tmp_path).unwrap();
 	let child_tmp_path_c_string = CString::new(child_tmp_path.as_os_str().as_bytes()).unwrap();
 	let tmpfs_c_string = CString::new("tmpfs").unwrap();
 	let ret = unsafe {
@@ -306,22 +330,26 @@ fn pre_exec(
 		)
 	};
 	if ret != 0 {
-		bail!(anyhow!(std::io::Error::last_os_error()).context("Failed to create /tmp."));
+		return Err(SandboxIncomplete::MountFailed(
+			std::io::Error::last_os_error(),
+			PathBuf::from("/tmp"),
+		)
+		.into());
 	}
 
 	// Create /etc.
 	let child_etc_path = parent_child_root_path.join("etc");
-	std::fs::create_dir_all(&child_etc_path)?;
+	std::fs::create_dir_all(&child_etc_path).unwrap();
 
 	if command.enable_network_access {
 		// Copy resolv.conf to re-use DNS config from host.
-		std::fs::copy("/etc/resolv.conf", child_etc_path.join("resolv.conf"))?;
+		std::fs::copy("/etc/resolv.conf", child_etc_path.join("resolv.conf"))
+			.map_err(SandboxIncomplete::ResolvConf)?;
 	} else {
 		// Unshare the network namespace to disable network access.
 		let ret = unsafe { unshare(CLONE_NEWNET) };
 		if ret != 0 {
-			bail!(anyhow!(std::io::Error::last_os_error())
-				.context("Failed to unshare network namespace."));
+			return Err(SandboxIncomplete::UnshareNetwork(std::io::Error::last_os_error()).into());
 		}
 	}
 
@@ -350,14 +378,23 @@ fn pre_exec(
 			)
 		};
 		if ret != 0 {
-			bail!(anyhow!(std::io::Error::last_os_error())
-				.context("Failed to mount the builder path."));
+			return Err(SandboxIncomplete::MountFailed(
+				std::io::Error::last_os_error(),
+				parent_source_path.to_owned(),
+			)
+			.into());
 		}
 
 		// Remount as read-only if writes are disabled. Bind mounts can only be made read-only by mounting it normally then remounting in read-only mode.
 		match mode {
 			SandboxPathMode::Read => {
-				let current_mount_options = get_mountpoint_remount_options(&parent_target_path)?;
+				let current_mount_options = get_mountpoint_remount_options(&parent_target_path)
+					.map_err(|error| {
+						SandboxError::FailedToGetMountpointOptions(
+							error,
+							parent_target_path.clone(),
+						)
+					})?;
 
 				let ret = unsafe {
 					mount(
@@ -369,8 +406,11 @@ fn pre_exec(
 					)
 				};
 				if ret != 0 {
-					bail!(anyhow!(std::io::Error::last_os_error())
-						.context("Failed to re-mount the builder path as read-only."));
+					return Err(SandboxIncomplete::MountReadOnlyFailed(
+						std::io::Error::last_os_error(),
+						parent_source_path.to_owned(),
+					)
+					.into());
 				}
 			},
 			SandboxPathMode::ReadWrite | SandboxPathMode::ReadWriteCreate => {},
@@ -390,7 +430,7 @@ fn pre_exec(
 		)
 	};
 	if ret != 0 {
-		bail!(anyhow!(std::io::Error::last_os_error()).context("Failed to pivot the root."));
+		return Err(SandboxIncomplete::PivotRootFailed(std::io::Error::last_os_error()).into());
 	}
 
 	// Change the current directory to the child root path.
@@ -398,7 +438,7 @@ fn pre_exec(
 	let child_root_path_c_string = CString::new(child_root_path.as_os_str().as_bytes()).unwrap();
 	let ret = unsafe { chdir(child_root_path_c_string.as_ptr()) };
 	if ret != 0 {
-		bail!(anyhow!(std::io::Error::last_os_error()).context("Failed to chdir."));
+		return Err(SandboxIncomplete::ChdirFailed(std::io::Error::last_os_error()).into());
 	}
 
 	// Unmount the parent's root.
@@ -406,22 +446,66 @@ fn pre_exec(
 		CString::new(child_parent_mount_path.as_os_str().as_bytes()).unwrap();
 	let ret = unsafe { umount2(child_parent_mount_path_c_string.as_ptr(), MNT_DETACH) };
 	if ret != 0 {
-		bail!(anyhow!(std::io::Error::last_os_error())
-			.context("Failed to unmount the parent's root."));
+		return Err(SandboxIncomplete::UnmountParentFailed(std::io::Error::last_os_error()).into());
 	}
 
 	// Remove the mountpoint for the parent's root.
 	let ret = unsafe { rmdir(child_parent_mount_path_c_string.as_ptr()) };
 	if ret != 0 {
-		bail!(anyhow!(std::io::Error::last_os_error())
-			.context("Failed to remove the mountpoint for the parent's root."));
+		return Err(SandboxIncomplete::RemoveParentFailed(std::io::Error::last_os_error()).into());
 	}
 
 	Ok(())
 }
 
+#[derive(Debug, thiserror::Error)]
+enum SandboxIncomplete {
+	#[error("Failed to unshare the user namespace.")]
+	UnshareUser(#[source] std::io::Error),
+	#[error("Failed to set the uid.")]
+	SetUidFailed(#[source] std::io::Error),
+	#[error("Failed to set the gid.")]
+	SetGidFailed(#[source] std::io::Error),
+	#[error("Failed to unshare the mount namespace.")]
+	UnshareMount(#[source] std::io::Error),
+	#[error("Failed to ensure the root does not have shared propagation.")]
+	MountPrivateRoot(#[source] std::io::Error),
+	#[error("Failed to ensure the new root is a mount point.")]
+	MountBindRoot(#[source] std::io::Error),
+	#[error("Failed to mount {1:?}.")]
+	MountFailed(#[source] std::io::Error, PathBuf),
+	#[error("Failed to mount {1:?} as read-only.")]
+	MountReadOnlyFailed(#[source] std::io::Error, PathBuf),
+	#[error("Failed to pivot the root.")]
+	PivotRootFailed(#[source] std::io::Error),
+	#[error("Failed to chdir.")]
+	ChdirFailed(#[source] std::io::Error),
+	#[error("Failed to unmount the parent's root.")]
+	UnmountParentFailed(#[source] std::io::Error),
+	#[error("Failed to remove the mountpoint for the parent's root.")]
+	RemoveParentFailed(#[source] std::io::Error),
+	#[error("Failed to unshare the network namespace.")]
+	UnshareNetwork(#[source] std::io::Error),
+	#[error("Failed to get resolv.conf from host.")]
+	ResolvConf(#[source] std::io::Error),
+}
+
+#[derive(Debug, thiserror::Error)]
+enum SandboxError {
+	#[error("Failed to send the message to the parent process that the UID and GID maps are ready to be set.")]
+	FailedToSendUidMapMessage(#[source] std::io::Error),
+	#[error("Failed to receive the message from the parent process that the UID and GID maps have been set.")]
+	FailedToReceiveUidMapMessage(#[source] std::io::Error),
+	#[error("Failed to get options for mountpoint {1:?}.")]
+	FailedToGetMountpointOptions(#[source] anyhow::Error, PathBuf),
+	#[error("Failed to set up sandbox.")]
+	Incomplete(#[from] SandboxIncomplete),
+}
+
 // Get the current options associated with a mountpoint that affect remounting. In some cases, when calling `mount` targeting an existing bind mount with the `MS_REMOUNT` option set, some extra options from the mountpoint underlying the bind mount need to be included too. Not including some extra options can cause the `mount` call to fail, such as the `MS_NODEV` option when inside a Podman container. This function parses the mount table at `/proc/mount` to get the current mount options. See mount(2) for a list of mount options that are used when remounting.
 fn get_mountpoint_remount_options(mountpoint: &Path) -> Result<c_ulong> {
+	return Ok(0);
+
 	// Read the mount table for the current mount namespace. This file uses the same format as fstab.
 	let mounts_table = std::fs::File::open("/proc/mounts")?;
 	let mounts_table = std::io::BufReader::new(mounts_table);
