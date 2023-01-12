3d0bde60cbb7601f285a036bc8a3403c33ff241a
David Yamnitsky <david@yamnitsky.com>
2023-01-12 11:36:51 -0500

    fix lsp

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![warn(clippy::pedantic)]
 #![allow(clippy::missing_errors_doc)]
 #![allow(clippy::missing_panics_doc)]
+#![allow(clippy::module_name_repetitions)]
 
 use anyhow::Result;
 use clap::Parser;
