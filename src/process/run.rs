9b021153b62d7d7f97e54707dc459d9f1e87860d
Ben Lovy <ben@deciduously.com>
2023-05-11 16:52:44 -0400

    Mount whole output temp, pass host path to check_in

--- src/process/run.rs
+++ src/process/run.rs
@@ -47,15 +47,13 @@ impl Process {
 		tokio::fs::create_dir_all(output_temp.path())
 			.await
 			.wrap_err("Failed to create the directory for the output.")?;
-		let output_temp_path = output_temp.path().join("output");
-		let output_temp_path = if cfg!(target_os = "macos") {
-			output_temp_path
+		let output_temp_path_host = output_temp.path().join("output");
+		let output_temp_path_guest = if cfg!(target_os = "macos") {
+			output_temp_path_host.clone()
 		} else if cfg!(target_os = "linux") {
-			let stripped = output_temp_path
-				.strip_prefix(tg.path())
-				.map_err(|_| {
-					error!("Unable to strip Tangram directory from process output temp directory")
-				})?;
+			let stripped = output_temp_path_host.strip_prefix(tg.path()).map_err(|_| {
+				error!("Unable to strip Tangram directory from process output temp directory")
+			})?;
 			fs::PathBuf::from("/.tangram").join(stripped)
 		} else {
 			unreachable!()
@@ -74,13 +72,18 @@ impl Process {
 		};
 
 		// Render the command template.
-		let command = render(&self.executable, &artifacts_directory, &output_temp_path).await?;
+		let command = render(
+			&self.executable,
+			&artifacts_directory,
+			&output_temp_path_guest,
+		)
+		.await?;
 
 		// Render the env templates.
 		let mut env: std::collections::BTreeMap<String, String> =
 			try_join_all(self.env.iter().map(|(key, value)| {
 				let artifacts_directory = &artifacts_directory;
-				let output_temp_path = &output_temp_path;
+				let output_temp_path = &output_temp_path_guest;
 				async move {
 					let key = key.clone();
 					let value = render(value, artifacts_directory, output_temp_path).await?;
@@ -94,14 +97,14 @@ impl Process {
 		// Set `TG_PLACEHOLDER_OUTPUT`.
 		env.insert(
 			"TANGRAM_PLACEHOLDER_OUTPUT".to_string(),
-			output_temp_path.display().to_string(),
+			output_temp_path_guest.display().to_string(),
 		);
 
 		// Render the args templates.
 		let args = try_join_all(
 			self.args
 				.iter()
-				.map(|arg| render(arg, &artifacts_directory, &output_temp_path)),
+				.map(|arg| render(arg, &artifacts_directory, &output_temp_path_guest)),
 		)
 		.await?;
 
@@ -131,8 +134,8 @@ impl Process {
 
 		// Add the output temp to the paths.
 		paths.insert(Path {
-			host_path: output_temp.path().to_owned(),
-			guest_path: output_temp_path.clone(),
+			host_path: output_temp_path_host.parent().unwrap().to_owned(),
+			guest_path: output_temp_path_guest.parent().unwrap().to_owned(),
 			mode: Mode::ReadWrite,
 			kind: Kind::Directory,
 		});
@@ -216,10 +219,10 @@ impl Process {
 		}
 		.await?;
 
-		tracing::debug!(?output_temp_path, "Checking in the process output.");
+		tracing::debug!(?output_temp_path_host, "Checking in the process output.");
 
 		// Check in the output temp.
-		let artifact = Artifact::check_in(tg, &output_temp_path)
+		let artifact = Artifact::check_in(tg, &output_temp_path_host)
 			.await
 			.wrap_err("Failed to check in the output.")?;
 
