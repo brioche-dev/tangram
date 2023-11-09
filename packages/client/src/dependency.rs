bada60a6ea0960bb0e8d696aa43dc893d987086a
m-hilgendorf <mike@hilgendorf.audio>
2023-11-09 12:04:34 -0600

    Add island to tg::Dependency, update parsing.

--- packages/client/src/dependency.rs
+++ packages/client/src/dependency.rs
@@ -1,3 +1,6 @@
+use itertools::Itertools;
+use tangram_error::{error, return_error};
+
 use crate::{Error, Relpath, Result};
 
 /// A dependency.
@@ -14,6 +17,9 @@ pub struct Dependency {
 
 	/// The package's path.
 	pub path: Option<Relpath>,
+
+	/// Whether or not this dependency is an island.
+	pub island: Option<bool>,
 }
 
 impl Dependency {
@@ -23,6 +29,7 @@ impl Dependency {
 			name: None,
 			version: None,
 			path: Some(path),
+			island: None,
 		}
 	}
 
@@ -32,23 +39,26 @@ impl Dependency {
 			name: Some(name),
 			version,
 			path: None,
+			island: None,
 		}
 	}
 }
 
 impl std::fmt::Display for Dependency {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		match (&self.name, &self.path) {
-			(Some(name), None) => {
-				write!(f, "{name}")?;
-				if let Some(version) = &self.version {
-					write!(f, "@{version}")?;
-				}
-				Ok(())
-			},
-			(None, Some(path)) => write!(f, "{path}"),
-			_ => unreachable!(),
+		let name_and_version = [
+			self.name.as_deref(),
+			self.version.as_deref(),
+		].into_iter().flatten().join("@");
+		let parameters = [
+			self.path.as_ref().map(|s| format!("path=./{s}")),
+			self.island.as_ref().map(|s| format!("island={s}"))
+		].into_iter().flatten().join(",");
+		write!(f, "{name_and_version}")?;
+		if !parameters.is_empty() {
+			write!(f, "?{parameters}")?;
 		}
+		Ok(())
 	}
 }
 
@@ -56,25 +66,48 @@ impl std::str::FromStr for Dependency {
 	type Err = Error;
 
 	fn from_str(value: &str) -> Result<Dependency> {
-		if value.starts_with('.') {
-			let path = value.parse()?;
-			Ok(Self {
-				name: None,
-				version: None,
-				path: Some(path),
-			})
-		} else {
-			// Otherwise, parse the string as a registry dependency.
-			let mut components = value.split('@');
-			let name = components.next().unwrap().to_owned();
-			let version = components.next().map(ToOwned::to_owned);
+		// Syntax: <name>@<version>?path=<path>,island=<boolean>
+		let mut components = value.split('?');
+		let mut name_and_version = components.next().unwrap().split('@');
+		let parameters = components
+			.next()
+			.into_iter()
+			.flat_map(|s| {
+				let parameters = s.split(',').map(|s| -> Result<(&str, &str)> {
+					let mut components = s.split('=');
+					let name = components.next().unwrap();
+					let value = components.next().ok_or(error!("Expected a value."))?;
+					Ok((name, value))
+				});
+				parameters
+			});
 
-			Ok(Self {
-				name: Some(name),
-				version,
-				path: None,
-			})
+		let name = match name_and_version.next() {
+			Some(name) if name.is_empty() => None,
+			Some(name) => Some(name.into()),
+			None => None,
+		};
+
+		let version = name_and_version.next().map(String::from);
+		let mut path = None;
+		let mut island = None;
+
+		for parameter in parameters {
+			let (name, value) = parameter?;
+			match name {
+				"path" => {
+					path = Some(value.parse()?);
+				}
+				"island" => {
+					island = Some(value.parse().map_err(|_| error!("Expected a boolean."))?);
+				}
+				name => {
+					return_error!("Unknown parameter: {name}.");
+				}
+			}
 		}
+
+		Ok(Dependency { name, version, path, island })
 	}
 }
 
@@ -89,5 +122,102 @@ impl TryFrom<String> for Dependency {
 impl From<Dependency> for String {
 	fn from(value: Dependency) -> Self {
 		value.to_string()
+	}
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Dependency;
+
+	#[test]
+	fn display() {
+		let dependency = Dependency {
+			name: Some("foo".into()),
+			version: None,
+			path: None,
+			island: None
+		};
+		assert_eq!(dependency.to_string(), "foo");
+		let dependency = Dependency {
+			name: Some("foo".into()),
+			version: Some("1.2.3".into()),
+			path: None,
+			island: None
+		};
+		assert_eq!(dependency.to_string(), "foo@1.2.3");
+		let dependency = Dependency {
+			name: Some("foo".into()),
+			version: None,
+			path: None,
+			island: None
+		};
+		assert_eq!(dependency.to_string(), "foo");
+		let dependency = Dependency {
+			name: Some("foo".into()),
+			version: Some("1.2.3".into()),
+			path: Some("./path/to/foo".parse().unwrap()),
+			island: None
+		};
+		assert_eq!(dependency.to_string(), "foo@1.2.3?path=./path/to/foo");
+		let dependency = Dependency {
+			name: Some("foo".into()),
+			version: Some("1.2.3".into()),
+			path: Some("./path/to/foo".parse().unwrap()),
+			island: Some(true)
+		};
+		assert_eq!(dependency.to_string(), "foo@1.2.3?path=./path/to/foo,island=true");
+		let dependency = Dependency {
+			name: None,
+			version: None,
+			path: Some("./path/to/foo".parse().unwrap()),
+			island: None,
+		};
+		assert_eq!(dependency.to_string(), "?path=./path/to/foo");
+	}
+
+	#[test]
+	fn parse() {
+		let dependency = Dependency {
+			name: Some("foo".into()),
+			version: None,
+			path: None,
+			island: None
+		};
+		assert_eq!(dependency, "foo".parse().unwrap());
+		let dependency = Dependency {
+			name: Some("foo".into()),
+			version: Some("1.2.3".into()),
+			path: None,
+			island: None
+		};
+		assert_eq!(dependency, "foo@1.2.3".parse().unwrap());
+		let dependency = Dependency {
+			name: Some("foo".into()),
+			version: None,
+			path: None,
+			island: None
+		};
+		assert_eq!(dependency, "foo".parse().unwrap());
+		let dependency = Dependency {
+			name: Some("foo".into()),
+			version: Some("1.2.3".into()),
+			path: Some("./path/to/foo".parse().unwrap()),
+			island: None
+		};
+		assert_eq!(dependency, "foo@1.2.3?path=./path/to/foo".parse().unwrap());
+		let dependency = Dependency {
+			name: Some("foo".into()),
+			version: Some("1.2.3".into()),
+			path: Some("./path/to/foo".parse().unwrap()),
+			island: Some(true)
+		};
+		assert_eq!(dependency, "foo@1.2.3?path=./path/to/foo,island=true".parse().unwrap());
+		let dependency = Dependency {
+			name: None,
+			version: None,
+			path: Some("./path/to/foo".parse().unwrap()),
+			island: None,
+		};
+		assert_eq!(dependency, "?path=./path/to/foo".parse().unwrap());
 	}
 }
--- packages/lsp/src/import.rs
+++ packages/lsp/src/import.rs
@@ -50,9 +50,9 @@ impl std::str::FromStr for Import {
 
 			if url.scheme() == "tangram" {
 				// Parse the URL's path as a dependency.
-				let dependency = url.path().parse().wrap_err_with(|| {
-					let path = url.path();
-					format!(r#"Failed to parse "{path}" as a dependency."#)
+				let value = &value["tangram:".len()..];
+				let dependency = value.parse().wrap_err_with(|| {
+					format!(r#"Failed to parse "{value}" as a dependency."#)
 				})?;
 				Ok(Import::Dependency(dependency))
 			} else {
