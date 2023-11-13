bc1d662410cd752daa0b1853fcb4f38a87b45e91
m-hilgendorf <mike@hilgendorf.audio>
2023-11-13 14:53:57 -0600

    Implement tg update.

--- packages/cli/src/commands/update.rs
+++ packages/cli/src/commands/update.rs
@@ -1,17 +1,23 @@
 use crate::Cli;
 use std::path::PathBuf;
-use tangram_error::{return_error, Result};
+use tangram_client::package::Builder;
+use tangram_error::Result;
 
 /// Update a package's dependencies.
 #[derive(Debug, clap::Args)]
 #[command(verbatim_doc_comment)]
 pub struct Args {
-	pub path: Option<PathBuf>,
+	#[arg(short, long, default_value = ".")]
+	pub path: PathBuf,
 }
 
 impl Cli {
 	#[allow(clippy::unused_async)]
-	pub async fn command_update(&self, _args: Args) -> Result<()> {
-		return_error!("This command is not yet implemented.");
+	pub async fn command_update(&self, args: Args) -> Result<()> {
+		let client = self.client().await?;
+		let client = client.as_ref();
+		let mut builder = tangram_package::Builder::new(&args.path);
+		builder.update(client, None).await?;
+		Ok(())
 	}
 }
