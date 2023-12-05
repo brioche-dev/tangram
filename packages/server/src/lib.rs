5fcb7cbc0b9ead49239098d6747abbdcd6666e3a
David Yamnitsky <david@yamnitsky.com>
2023-12-04 22:55:57 -0500

    create packages module in server

--- packages/server/src/lib.rs
+++ packages/server/src/lib.rs
@@ -11,13 +11,14 @@ use std::{
 use tangram_client as tg;
 use tangram_error::{Result, Wrap, WrapErr};
 use tangram_package::Ext;
-use tg::util::rmrf;
+use tg::{dependency, util::rmrf, Handle};
 
 mod build;
 mod clean;
 mod database;
 mod migrations;
 mod object;
+mod package;
 mod pool;
 mod serve;
 
@@ -488,79 +489,43 @@ impl tg::Handle for Server {
 		&self,
 		dependency: &tg::Dependency,
 	) -> Result<(tg::directory::Id, tg::lock::Id)> {
-		let (package, lock) = tangram_package::new(self, dependency).await?;
-		Ok((
-			package.id(self).await?.clone(),
-			lock.id(self).await?.clone(),
-		))
+		self.create_package_and_lock(dependency).await
 	}
 
 	async fn search_packages(&self, query: &str) -> Result<Vec<String>> {
-		self.inner
-			.remote
-			.as_ref()
-			.wrap_err("The server does not have a remote.")?
-			.search_packages(query)
-			.await
+		self.search_packages(query).await
 	}
 
 	async fn try_get_package(
 		&self,
 		dependency: &tg::Dependency,
 	) -> Result<Option<tg::directory::Id>> {
-		if let Some(id) = dependency.id.as_ref() {
-			return Ok(Some(id.clone()));
-		}
-
-		self.inner
-			.remote
-			.as_ref()
-			.wrap_err("The server does not have a remote.")?
-			.try_get_package(dependency)
-			.await
+		self.try_get_package(dependency).await
 	}
 
 	async fn try_get_package_versions(
 		&self,
 		dependency: &tg::Dependency,
 	) -> Result<Option<Vec<String>>> {
-		self.inner
-			.remote
-			.as_ref()
-			.wrap_err("The server does not have a remote.")?
-			.try_get_package_versions(dependency)
-			.await
+		self.try_get_package_versions(dependency).await
 	}
 
 	async fn try_get_package_metadata(
 		&self,
 		dependency: &tg::Dependency,
 	) -> Result<Option<tg::package::Metadata>> {
-		let package = tg::Directory::with_id(self.get_package(dependency).await?);
-		let metadata = package.metadata(self).await?;
-		Ok(Some(metadata))
+		self.try_get_package_metadata(dependency).await
 	}
 
 	async fn try_get_package_dependencies(
 		&self,
 		dependency: &tg::Dependency,
 	) -> Result<Option<Vec<tg::Dependency>>> {
-		let package = tg::Directory::with_id(self.get_package(dependency).await?);
-		let dependencies = package.dependencies(self).await?;
-		Ok(Some(dependencies))
+		self.try_get_package_dependencies(dependency).await
 	}
 
 	async fn publish_package(&self, user: Option<&tg::User>, id: &tg::directory::Id) -> Result<()> {
-		let remote = self
-			.inner
-			.remote
-			.as_ref()
-			.wrap_err("The server does not have a remote.")?;
-		tg::object::Handle::with_id(id.clone().into())
-			.push(self, remote.as_ref())
-			.await
-			.wrap_err("Failed to push the package.")?;
-		remote.publish_package(user, id).await
+		self.publish_package(user, id).await
 	}
 
 	async fn create_login(&self) -> Result<tg::user::Login> {
