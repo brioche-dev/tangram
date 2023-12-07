3cb0a19f2f054aecd1589ab1d9631bbaeaae5eaf
Ben Lovy <ben@deciduously.com>
2023-12-06 19:20:20 -0500

    Fix template undrender regex
    * Make prefix group non-capturing, remove unused group

--- packages/client/src/template.rs
+++ packages/client/src/template.rs
@@ -61,7 +61,7 @@ impl Template {
 			.iter()
 			.map(|artifacts_path| artifacts_path.to_str().unwrap().replace('.', r"\."))
 			.join("|");
-		let regex = format!(r"(?:{artifacts_paths})/((fil_|dir_|sym_)01[a-z2-7]{{52}}))");
+		let regex = format!(r"(?:{artifacts_paths})/((?:fil_|dir_|sym_)01[a-z2-7]{{52}})");
 		let regex = regex::Regex::new(&regex).unwrap();
 
 		let mut i = 0;
