1f9d1e3a033682f3445ed33715cd0afd34fd6575
Ben Lovy <ben@deciduously.com>
2023-12-12 12:12:28 -0500

    fix default tg env executable path

--- packages/cli/src/commands/env.rs
+++ packages/cli/src/commands/env.rs
@@ -28,11 +28,11 @@ pub struct Args {
 
 impl Cli {
 	pub async fn command_env(&self, mut args: Args) -> Result<()> {
-		// Set the executable path to `.tangram/env` if it is not set.
+		// Set the executable path to `.tangram/run` if it is not set.
 		args.run_args.executable_path = Some(
 			args.run_args
 				.executable_path
-				.unwrap_or_else(|| ".tangram/env".parse().unwrap()),
+				.unwrap_or_else(|| ".tangram/run".parse().unwrap()),
 		);
 
 		// Create the run args.
